c31731e3d32ffc34202b133acfa9272da240c3a2
Brennan Iannandrea <brennan.iannandrea@gmail.com>
2018-04-13 05:13:20 -0400

    Uploading work for calculator application (unfinished)

---
+++ Test/Test/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculator_I_guess = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Calculator_I_guess
+            // 
+            this.Calculator_I_guess.AutoSize = true;
+            this.Calculator_I_guess.Location = new System.Drawing.Point(12, 9);
+            this.Calculator_I_guess.Name = "Calculator_I_guess";
+            this.Calculator_I_guess.Size = new System.Drawing.Size(91, 13);
+            this.Calculator_I_guess.TabIndex = 0;
+            this.Calculator_I_guess.Text = "Calculator I guess";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(309, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Enter += new System.EventHandler(this.calculate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 552);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Calculator_I_guess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Calculator_I_guess;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Test/Test/Form1.cs
@@ -0,0 +1,85 @@
+﻿//@author Brennan Iannandrea
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate(object sender, EventArgs e)
+        {
+            bool checkWholebox;
+            string a = textBox1.Text;
+            foreach(char element in a)
+            {
+                if (!IsNumeric(element)|| element.ToString() !="/" || element.ToString() != "*" || element.ToString() != "." || element.ToString() != "+" || element.ToString() != "-")
+                {
+                    checkWholebox = false;
+                }
+            }
+            if (checkWholebox = false){
+                while (a.IndexOf("/") != -1 || a.IndexOf("*") != -1)
+                {
+                if (a.IndexOf("/") != 1 && a.IndexOf("*") == -1)
+                    {
+                        if (a.IndexOf("/") < a.IndexOf("*"))
+                        {
+                            int mid = a.IndexOf("/");
+                            bool checkfir = false;
+                            int findfir = mid;
+                            while (checkfir == false)
+                            {
+                                findfir--;
+                                if (!IsNumeric(a[findfir]) && a[findfir].ToString() != ("."))
+                                {
+                                    checkfir = true;
+                                }
+                            }
+                            bool checklas = false;
+                            int findlas = mid;
+                            while (checkfir == false)
+                            {
+                                findlas++;
+                                if (!IsNumeric(a[findlas]) && a[findlas].ToString() != ("."))
+                                {
+                                    checklas = true;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            int mid = a.IndexOf("*");
+
+                        }
+                    }
+                }
+            }
+        }
+        private bool IsNumeric(string a)
+        {
+            return int.TryParse(a, out int bla);
+        }
+        private bool IsNumeric(char a)
+        {
+            string b = a.ToString();
+            return int.TryParse(b, out int bla);
+        }
+        private bool ValidCheck(string a)
+        {
+
+        }
+    }
+}
